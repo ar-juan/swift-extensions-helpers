7eeba8828f2b8475d9fa2e7c05cf27a8230e5f90
Arjan van der Laan <avdlaan85@hotmail.com>
2016-04-21 17:45:43 +0200

    NSManagedObject + Create

--- Extensions/NSManagedObject/NSManagedObject+Create.swift
+++ Extensions/NSManagedObject/NSManagedObject+Create.swift
@@ -10,20 +10,28 @@ import Foundation
 import CoreData
 
 extension NSManagedObject {
-    class func objectWithRemoteId<T: NSManagedObject>(remoteId: Int, inContext context: NSManagedObjectContext) -> T? {
-        var object: T? = nil
+    
+    // stackoverflow.com/questions/27109268/how-can-i-create-instances-of-managed-object-subclasses-in-a-nsmanagedobject-swi
+    convenience init(context: NSManagedObjectContext) {
+        let entity = NSEntityDescription.entityForName(String(self.dynamicType), inManagedObjectContext: context)!
+        self.init(entity: entity, insertIntoManagedObjectContext: context)
+    }
+    
+    // stackoverflow.com/questions/25271208/cast-to-typeofself
+    class func objectWithRemoteId<T where T: NSManagedObject>(remoteId: Int, inContext context: NSManagedObjectContext) -> T? {
+        var object: T?
         if remoteId > 0 {
-            let request = NSFetchRequest(entityName: String(T))
+            let request = NSFetchRequest(entityName: String(self))
             request.predicate = NSPredicate(format: "id == %d", remoteId)
             
             do
             {
                 let results = try context.executeFetchRequest(request)
-                if results.count > 1 { logthis("More than one \(String(T)) with same id. Should never happen") }
-                else if let result = results.first as? T { object = result }
+                if results.count > 1 { logthis("More than one \(String(self)) with same id. Should never happen") }
+                else if let result = results.first as? NSManagedObject { object = result as? T }
                 else {
-                    object = (NSEntityDescription.insertNewObjectForEntityForName(String(T), inManagedObjectContext: context) as! T)
-                    object?.setValue(remoteId, forKeyPath: "id") //object!.id = remoteId
+                    object = (NSEntityDescription.insertNewObjectForEntityForName(String(self), inManagedObjectContext: context) as! T)
+                    object?.setValue(remoteId, forKeyPath: "id")
                 }
             }
             catch let error as NSError
