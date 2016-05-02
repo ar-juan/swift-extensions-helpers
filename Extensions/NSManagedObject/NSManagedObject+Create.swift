c9fb2707ccaa95a67fe8841dbcb97a1f018094c5
Arjan van der Laan <avdlaan85@hotmail.com>
2016-05-02 09:24:08 +0200

    nextNSNumberIntForField method added

--- Extensions/NSManagedObject/NSManagedObject+Create.swift
+++ Extensions/NSManagedObject/NSManagedObject+Create.swift
@@ -18,11 +18,11 @@ extension NSManagedObject {
     }
     
     // stackoverflow.com/questions/25271208/cast-to-typeofself
-    class func objectWithRemoteId<T where T: NSManagedObject>(remoteId: Int, inContext context: NSManagedObjectContext) -> T? {
+    class func objectWithId<T where T: NSManagedObject>(id: Int, inContext context: NSManagedObjectContext) -> T? {
         var object: T?
-        if remoteId > 0 {
+        if id > 0 {
             let request = NSFetchRequest(entityName: String(self))
-            request.predicate = NSPredicate(format: "id == %d", remoteId)
+            request.predicate = NSPredicate(format: "id == %d", id)
             
             do
             {
@@ -31,7 +31,7 @@ extension NSManagedObject {
                 else if let result = results.first as? NSManagedObject { object = result as? T }
                 else {
                     object = (NSEntityDescription.insertNewObjectForEntityForName(String(self), inManagedObjectContext: context) as! T)
-                    object?.setValue(remoteId, forKeyPath: "id")
+                    object?.setValue(id, forKeyPath: "id")
                 }
             }
             catch let error as NSError
@@ -40,5 +40,35 @@ extension NSManagedObject {
             }
         }
         return object
+    }
+    
+    class func nextNSNumberIntForField(field: String, context: NSManagedObjectContext) -> NSNumber? {
+        var nextNumber: NSNumber? = nil
+        
+        let request = NSFetchRequest(entityName: String(self))
+        request.sortDescriptors = [NSSortDescriptor(key: field, ascending: true)]
+        
+        do
+        {
+            let results = try context.executeFetchRequest(request)
+            if results.count == 0 {
+                nextNumber = 1
+            } else {
+
+                let last = results.last as! NSManagedObject
+                
+                guard let lastNumber = last.valueForKeyPath(field) as? NSNumber else {
+                    logthis("\(field) is not a NSNumber (wrong input) or is nil (model integrity compromised)")
+                    return nil
+                }
+                nextNumber = lastNumber.integerValue + 1
+            }
+        }
+        catch let error as NSError
+        {
+            logthis("\(error.localizedDescription) (\(error.localizedFailureReason))")
+        }
+        
+        return nextNumber
     }
 }
