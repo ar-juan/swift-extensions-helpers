3dd927f4f5eefaf574cfa49746078161839dd186
Arjan van der Laan <avdlaan85@hotmail.com>
2016-12-21 21:07:57 +0100

    uicolor extension hex string

--- Extensions/UIColor/Initialization/UIColor.swift
+++ Extensions/UIColor/Initialization/UIColor.swift
@@ -29,4 +29,27 @@ extension UIColor {
     convenience init(netHex:Int) {
         self.init(red:(netHex >> 16) & 0xff, green:(netHex >> 8) & 0xff, blue:netHex & 0xff)
     }
+    
+    
+    convenience init(hexString: String) {
+        var cString:String = hexString.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+
+        if ((cString.characters.count) != 6) {
+            self.init(white: 0.5, alpha: 1)
+        } else {
+            var rgbValue:UInt32 = 0
+            Scanner(string: cString).scanHexInt32(&rgbValue)
+            
+            self.init(
+                red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                alpha: CGFloat(1.0)
+            )
+        }
+    }
 }
